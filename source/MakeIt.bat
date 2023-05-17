cb38a646136217bcd2cb7ececc74f1c330946f5c
Gavin Holt <holtgavin@gmail.com>
2023-05-17 20:20:23 +0000

    Update MakeIt.bat

--- source/MakeIt.bat
+++ source/MakeIt.bat
@@ -8,9 +8,6 @@ REM The bin stub is already UPX compressed!
 REM Compile APEditor
 Ahk2Exe.exe /in APEditor.ahk /out APEditor.exe /icon img\APEditor.ico
 
-REM Compile APRunner
-Ahk2Exe.exe /in APRunner.ahk /out APRunner.exe /icon img\APEditor.ico
-
 REM Start
 shelexec.exe /Params:%1 /EXE "APEditor.exe"
 exit
